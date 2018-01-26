21de8990fb4dd99fa8b6a3c852fccc9ff0b07601
alexisthual <alexisthual@gmail.com>
2018-01-26 17:54:19 +0100

    Eliminate frequency

--- tp1/answers.md
+++ tp1/answers.md
@@ -8,4 +8,14 @@
 
 # Estimation de la fréquence fondamentale par la méthode du produit spectral
 
-2.2 $H \frac{R-1}{N_{fft}}F_s \leq F_s / 2 \Leftrightarrow R \leq \frac{N_{fft}}{2H} + 1$
+2. 2. $H \frac{R-1}{N_{fft}}F_s \leq F_s / 2 \Leftrightarrow R \leq \frac{N_{fft}}{2H} + 1$
+
+# Soustraction du son correspondant à la fréquence fondamentale détectée
+
+3. 1. Pour le piano, on s'attend à ce que la fréquence fondamentale soit bien démarquée (percusion), il est raisonnable de penser qu'une imprécision de l'ordre de 1% suffit. Pour le hautbois, 5% pourrait probablement suffire.
+
+Frames par seconde $F_s$
+
+TTF : $N_{fft}$ fréquences : $\forall 0 \leq k \leq N_{fft}, \quad f=\frac{k}{N_{fft}} \mod 2\pi$
+
+Produit : $R$ valeurs avec $H(R-1) \leq N_{fft}$
--- tp1/main.py
+++ tp1/main.py
@@ -1,10 +1,13 @@
 import wave
 import numpy as np
 from math import floor
+import matplotlib.pyplot as plt
 
 class FrequencyEstimator():
     def __init__(self, frames, frameRate, verbose=False):
         self.verbose = verbose
+        self.colors = ["#2965CC", "#29A634", "#D99E0B", "#D13913"]
+
         self.Fmin = 50
         self.Fmax = 900
         self.H = 4
@@ -17,6 +20,21 @@ class FrequencyEstimator():
         offset = int(floor(0.1 * frameRate))
         self.x = np.array(frames[offset:offset + self.N]) * np.hamming(self.N)
         self.fft = np.fft.fft(self.x, n=self.Nfft)
+        self.fft = np.absolute(self.fft)
+
+        self.alpha = 0.02
+        self.beta = 0
+
+        if self.verbose:
+            self.plotSignal()
+
+    def plotSignal(self):
+        f, axarr = plt.subplots(1, 2, figsize=(15,5))
+        axarr[0].plot(self.x, color=self.colors[0])
+        axarr[1].plot(self.fft[10:], color=self.colors[0])
+        axarr[1].plot(np.mean(self.fft), color=self.colors[1])
+        axarr[1].set_yscale('log')
+        plt.show()
 
     def nextPower(self, n, p):
         '''Outputs the next power of p for n.'''
@@ -36,7 +54,14 @@ class FrequencyEstimator():
 
         for r in range(self.R):
             for h in range(self.H):
-                self.p[r] *= abs(self.x[r*h])
+                self.p[r] *= self.fft[r*h]
+
+        if self.verbose:
+            fig = plt.figure(figsize=(15,5))
+            # Eliminate frequency 0
+            plt.plot(self.p[1:], color=self.colors[0])
+            plt.yscale('log')
+            plt.show()
 
     def findMax(self):
         '''Computes the maximal frequency of the spectral product.'''
@@ -49,12 +74,38 @@ class FrequencyEstimator():
             print('Nmax: {}'.format(str(Nmax)))
 
         i = Nmin + np.argmax(self.p[Nmin:Nmax])
-        f = i / self.Nfft
+        f = i * self.frameRate / self.Nfft
+
+        if self.verbose:
+            print('Found frequency: {}'.format(str(f)))
 
         return f
 
+    def eliminateFrequency(self, f):
+        f1 = (1 - self.alpha) * f
+        f2 = (1 + self.alpha) * f
+
+        k1 = int(floor(f1 * self.Nfft / self.frameRate))
+        k2 = int(floor(f2 * self.Nfft / self.frameRate))
+
+        spikeLocation = k1 + np.argmax(self.p[k1:k2])
+        spikeSize = 50
+
+        if self.verbose:
+            print('f1 {} f2 {} k1 {} k2 {} spikeLocation {}'.format(str(f1), str(f2), str(k1), str(k2), str(spikeLocation)))
+            print('Corrected frequency: {}'.format(str(spikeLocation * self.frameRate / self.N)))
+
+        kf = spikeLocation
+        while kf < self.Nfft:
+            self.fft[spikeLocation - spikeSize/2:spikeLocation + spikeSize/2] = [np.min(self.fft[spikeLocation - spikeSize/2:spikeLocation + spikeSize/2])] * spikeSize
+            kf += spikeLocation
+
+    def shouldStop(self):
+        return False
+
+
 if __name__ == '__main__':
-    with wave.open('./sons_multipitch/A3_piano.wav') as s:
+    with wave.open('./tp1/sons_multipitch/A3_piano.wav') as s:
         frames = []
         frame = s.readframes(1)
         while frame:
@@ -66,6 +117,26 @@ if __name__ == '__main__':
         frameRate = s.getframerate()
         frequencyEstimator = FrequencyEstimator(frames, frameRate, verbose=True)
 
-        frequencyEstimator.computeProduct()
-        f0 = frequencyEstimator.findMax()
-        print('Frequence fondamentale : {}'.format(str(f0)))
+        frequencies = []
+        shouldStop = frequencyEstimator.shouldStop()
+
+        k = 0
+        while not shouldStop and k < 10:
+            frequencyEstimator.computeProduct()
+            f0 = frequencyEstimator.findMax()
+            frequencyEstimator.eliminateFrequency(f0)
+
+            frequencies.append(f0)
+            shouldStop = frequencyEstimator.shouldStop()
+            k += 1
+
+            print('Frequence fondamentale : {}'.format(str(f0)))
+
+        s.close()
+
+# %% Test cell
+import numpy as np
+
+a = [1,2,3,4]
+a[1:3] = [7,8]
+a
